b4c316ac0a57e8ad5b44c33e577932e8cbdd4434
Tina Eilertsen <tinaeilertsen@outlook.com>
2021-09-24 14:12:05 +0200

    Task A completed

---
+++ exercises/src/test/java/org/pg4200/ex05/BinaryTreeLeftMaxDeleteTest.java
@@ -0,0 +1,13 @@
+package org.pg4200.ex05;
+
+import org.pg4200.les05.MyMapBinarySearchTree;
+import org.pg4200.les05.MyMapBinarySearchTreeTest;
+import org.pg4200.les05.MyMapTreeBased;
+
+public class BinaryTreeLeftMaxDeleteTest extends MyMapBinarySearchTreeTest {
+
+    @Override
+    protected <K extends Comparable<K>, V> MyMapTreeBased<K, V> getTreeInstance() {
+        return new MyMapBinarySearchTree<>();
+    }
+}
